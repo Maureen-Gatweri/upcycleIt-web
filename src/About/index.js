d3467e2f60e7d5565be44248f437813e43b350c5
Maureen-Gatweri <maureennmwendwa@gmail.com>
2024-06-13 13:49:27 +0300

    add file

---
+++ src/About/index.js
@@ -0,0 +1,8 @@
+import './index.css'
+const About = ()=>{
+    return(
+        <div>
+            <p className="a">I enjoy upcycling</p>
+        </div>
+    )
+}
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import About from'./About'
+const About=()=>{
+  return(
+    <div>
+    <h2> Upcycle it Website</h2>
+    <About/>
+    
+    </div>
+  )
+}
+export default App;
